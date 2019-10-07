e2615fee97b29b8ab87ea8ac6e43188217f04cc2
John Dennehy <john.dennehy@gmail.com>
2019-10-07 16:49:20 +0100

    fixes bug

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,15 +1,14 @@
 require './app'
 
+
 feature 'Viewing bookmarks' do
   scenario 'A user can see bookmarks' do
+    connection = PG.connect(dbname: "bookmark_manager_test")
+    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.google.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.makersacademy.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.twitter.com');")
+    
     visit('/bookmarks')
-
-    connection = PG.connect(dbname: 'bookmark_manager_test')
-
-    # Add the test data
-    connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
-    connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
-    connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.twitter.com');")
 
     expect(page).to have_content "http://www.google.com"
     expect(page).to have_content "http://www.makersacademy.com"
