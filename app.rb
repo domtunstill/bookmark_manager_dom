f83c6ef32158a04c864c54558fd21d9fb875f5bf
John Dennehy <john.dennehy@gmail.com>
2019-10-07 13:52:28 +0100

    adds 1st feature test and simple list of bookmarks

--- app.rb
+++ app.rb
@@ -5,5 +5,15 @@ class BookmarkManager < Sinatra::Base
     'Hello World'
   end
 
+  get '/bookmarks' do
+    @bookmarks = [
+      "http://www.makersacademy.com",
+      "http://www.destroyallsoftware.com",
+      "http://www.google.com"
+    ]
+    @bookmarks.join
+    erb :'bookmarks/index'
+  end
+
   run! if app_file == $0
 end
